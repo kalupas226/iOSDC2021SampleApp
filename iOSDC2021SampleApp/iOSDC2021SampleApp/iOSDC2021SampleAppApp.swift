b4607d451776d02d833d0fe96747905047aa7d5e
Kenta Aikawa <kenta.aikawa@sansan.com>
2021-07-18 14:26:24 +0900

    Added: プロジェクト作成

---
+++ iOSDC2021SampleApp/iOSDC2021SampleApp/iOSDC2021SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSDC2021SampleAppApp.swift
+//  iOSDC2021SampleApp
+//
+//  Created by Aikawa Kenta on 2021/07/18.
+//
+
+import SwiftUI
+
+@main
+struct iOSDC2021SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOSDC2021SampleApp/iOSDC2021SampleAppTests/iOSDC2021SampleAppTests.swift
@@ -0,0 +1,33 @@
+//
+//  iOSDC2021SampleAppTests.swift
+//  iOSDC2021SampleAppTests
+//
+//  Created by Aikawa Kenta on 2021/07/18.
+//
+
+import XCTest
+@testable import iOSDC2021SampleApp
+
+class iOSDC2021SampleAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
